8147a97fbb412154f67916e6232e3c7874e8e70b
Sabrina Jain <62222927+sab747@users.noreply.github.com>
2021-10-13 16:14:12 -0400

    Initial App Scaffolding
    Initial app scaffolding. XCode project using the SwiftUI interface with a single demo SpriteKit GameScene

---
+++ Breeze/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Breeze
+//
+//  Created by Sabrina Jain on 10/13/21.
+//
+
+import SwiftUI
+import CoreData
+import SpriteKit
+
+
+struct ContentView: View {
+    
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .fill
+        return scene
+        
+    }
+    
+    var body: some View {
+        SpriteView(scene: scene)
+            .frame(width: 300, height: 400)
+            .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
