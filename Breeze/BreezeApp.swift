d53951d85032f5cf2104b9fc5c27f0c1309b79af
Sabrina Jain <62222927+sab747@users.noreply.github.com>
2021-10-21 17:52:58 -0400

    Connecting to AWS Amplify
    Connects app to AWS Amplify backend. Note that you will need to add the amplify ios package ("Add package"): https://github.com/aws-amplify/amplify-ios

--- Breeze/BreezeApp.swift
+++ Breeze/BreezeApp.swift
@@ -6,6 +6,26 @@
 //
 
 import SwiftUI
+import Amplify
+import AWSCognitoAuthPlugin
+import AWSAuthCore
+import AWSAPIPlugin
+import AWSDataStorePlugin
+
+func configureAmplify() {
+    let models = AmplifyModels()
+    let apiPlugin = AWSAPIPlugin(modelRegistration: models)
+    let dataStorePlugin = AWSDataStorePlugin(modelRegistration: models)
+    do {
+        try Amplify.add(plugin: apiPlugin)
+        try Amplify.add(plugin: dataStorePlugin)
+        try Amplify.configure()
+        print("Initialized Amplify");
+    } catch {
+        assert(false, "Could not initialize Amplify: \(error)")
+    }
+}
+
 
 @main
 struct BreezeApp: App {
@@ -16,4 +36,10 @@ struct BreezeApp: App {
             ContentView()
         }
     }
+    
+    
+    public init() {
+        configureAmplify()
+    }
+     
 }
