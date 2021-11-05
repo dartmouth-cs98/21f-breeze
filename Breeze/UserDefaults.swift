2d3ab46712f9e194c4eb9f1fd056fe1075ffbf77
Laurel Dernbach <laurel.k.dernbach.22@dartmouth.edu>
2021-11-04 21:21:29 -0400

    adding boolean for setup

--- Breeze/UserDefaults.swift
+++ Breeze/UserDefaults.swift
@@ -7,12 +7,17 @@
 
 import Foundation
 
+// getters and setters for stander UserDefaults data
 extension UserDefaults{
 
+    // example usage:
+    // UserDefaults.standard.setPoints(value: 90)
     func setPoints(value: Int) {
         set(value, forKey: UserDefaultsKeys.points.rawValue)
     }
 
+    // example usage:
+    // UserDefaults.standard.getPoints()
     func getPoints()-> Int {
         return integer(forKey: UserDefaultsKeys.points.rawValue)
     }
@@ -32,10 +37,20 @@ extension UserDefaults{
     func getTime() -> Int{
         return integer(forKey: UserDefaultsKeys.timeInMinutes.rawValue)
     }
+    
+    // needs to be initialized as false upon first app open
+    func setSetupBool(value: Bool) {
+        set(value, forKey: UserDefaultsKeys.setup.rawValue)
+    }
+
+    func isSetup()-> Bool {
+        return bool(forKey: UserDefaultsKeys.setup.rawValue)
+    }
 }
 
 enum UserDefaultsKeys : String {
     case points
     case streak
     case timeInMinutes
+    case setup
 }
