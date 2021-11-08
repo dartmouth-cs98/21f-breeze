7317439c8df710c6f17772928bea7fc42f476768
Grant Dumanian <grant.dumanian@mana.ai>
2021-11-08 16:16:17 -0500

    Added physics bodies to dock and boat, updated background, added first stab at a timer

--- Breeze/GameScene.swift
+++ Breeze/GameScene.swift
@@ -8,8 +8,17 @@ import GameplayKit
 import CoreMotion
 
 class GameScene: SKScene {
-    let boat = SKSpriteNode(imageNamed: "boat")
+    
+    var boat = SKSpriteNode(imageNamed: "boat")
     var dock = SKSpriteNode(imageNamed: "dock")
+    var levelTimerLabel = SKLabelNode(fontNamed: "ArialMT")
+
+    var levelTimerValue: Int = 10 {
+        didSet {
+            levelTimerLabel.text = "Time left: \(levelTimerValue)"
+        }
+    }
+    
     
     private let motionManager = CMMotionManager()
 
@@ -18,42 +27,71 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         motionManager.startAccelerometerUpdates()
         
+
         //background
         self.backgroundColor = SKColor.white
         
         // game scene physics
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
         
-        // boat node
-        boat.position = CGPoint(x: frame.midX, y: frame.minY)
+        // boat node attributes
+        boat.position = CGPoint(x: frame.midX + 5, y: frame.minY + 50)
         boat.size = CGSize(width: 100, height: 120)
         boat.removeFromParent()
         self.addChild(boat)
-        
-        dock.position = CGPoint(x: frame.midX - 58, y: frame.minY)
-        dock.size = CGSize(width: 100, height: 300)
-        self.addChild(dock)
-                      
         
         //boat physics
         boat.physicsBody = SKPhysicsBody(circleOfRadius: boat.size.width / 2)
         boat.physicsBody?.allowsRotation = false
         boat.physicsBody?.restitution = 0.5
         
-       
+        //dock node attributes
+        dock.position = CGPoint(x: frame.midX - 58, y: frame.minY)
+        dock.size = CGSize(width: 100, height: 300)
+        self.addChild(dock)
+                      
+        //dock physics
+        dock.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: dock.size.width - 60, height: dock.size.height))
+        dock.physicsBody?.allowsRotation = false
+        dock.physicsBody?.restitution = 0.5
+        dock.physicsBody?.isDynamic = false
+        
+        //level timer
+        levelTimerLabel.fontColor = SKColor.black
+        levelTimerLabel.fontSize = 40
+        levelTimerLabel.position = CGPoint(x: frame.midX, y: frame.midY)
+        levelTimerLabel.text = "Time left: \(levelTimerValue)"
+        self.addChild(levelTimerLabel)
+        
+        let wait = SKAction.wait(forDuration: 0.5)
+        let block = SKAction.run(_:)({
+               [unowned self] in
+
+               if self.levelTimerValue > 0{
+                   self.levelTimerValue -= 1
+                   print("working")
+               }else{
+                   self.removeAction(forKey: "countdown")
+               }
+           })
+           let sequence = SKAction.sequence([wait,block])
+
+        run(SKAction.repeatForever(sequence), withKey: "countdown")
+        
+//        pauseScene()
     }
     
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        let sceneTwo = SceneTwo(fileNamed: "SceneTwo")
-//        let transition = SKTransition.flipVertical(withDuration: 1.0)
-//        sceneTwo?.scaleMode = .aspectFill
-//        scene?.view?.presentScene(sceneTwo!, transition: transition)
-//    }
-//
+    
+    func pauseScene(){
+        scene?.physicsWorld.speed = 0
+    }
+    
     override func update(_ currentTime: TimeInterval) {
 
         if let accelerometerData = motionManager.accelerometerData {
             physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.x * 9.8, dy: accelerometerData.acceleration.y * 9.8)
         }
+        
+        if action(forKey: "countdown") != nil {removeAction(forKey: "countdown")}
     }
 }
