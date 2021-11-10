80c6d4fa0d2e0a96145339260e4000382bb6953a
John Weingart <jrweingart@gmail.com>
2021-11-10 17:07:52 -0500

    name change

--- Breeze/GameScene.swift
+++ Breeze/GameScene.swift
@@ -9,7 +9,7 @@ import CoreMotion
 
 class GameScene: SKScene {
     
-    var boat = SKSpriteNode(imageNamed: "Boat")
+    var boat = SKSpriteNode(imageNamed: "boat")
     var dock = SKSpriteNode(imageNamed: "dock")
     
     var timer: Timer?
