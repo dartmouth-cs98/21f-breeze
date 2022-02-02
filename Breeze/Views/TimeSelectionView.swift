ba4d359e8342aad6f217086b00bdd6847d36422d
Breeze <Breeze@Sabrinas-MacBook-Pro-4.local>
2022-02-02 16:22:58 -0500

    time picker button disabled until tap twice

--- Breeze/Views/TimeSelectionView.swift
+++ Breeze/Views/TimeSelectionView.swift
@@ -10,26 +10,38 @@ import SwiftUI
 struct TimeSelectionView: View {
     @Binding var timeSelectionIsPresenting: Bool
     @State private var startTime = Time(hours:0, minutes:0, seconds:0)
+    @State private var timeSelected = false;
 
     var body: some View {
         ZStack {
            VStack {
-             Text("Breeze will notify you every...")
+               Text("Breeze will notify you every...")
                    .font(Font.custom("Baloo2-Regular", size:20))
                    .multilineTextAlignment(.center)
-             DurationPickerView(time: $startTime)
-             Button("Confirm", action: selectTime)
-                .font(Font.custom("Baloo2-Regular", size:20))
-                .background(Color.init(UIColor(red: 221/255, green: 247/255, blue: 246/255, alpha: 1)))
-                .foregroundColor(Color.black)
-                .cornerRadius(6)
-                .padding()
+               DurationPickerView(time: $startTime, changeTriggered: $timeSelected)
+               // if user has touched the durationPicker twice (work around to apple's bug)
+               if (timeSelected) {
+                   Button("Confirm", action: selectTime)
+                       .font(Font.custom("Baloo2-Regular", size:20))
+                       .background(Color.init(UIColor(red: 221/255, green: 247/255, blue: 246/255, alpha: 1)))
+                       .foregroundColor(Color.black)
+                       .cornerRadius(6)
+                       .padding()
+               }
+               // otherwise, unclickable button
+               else {
+                   Button("Confirm", action: {})
+                       .font(Font.custom("Baloo2-Regular", size:20))
+                       .background(Color.init(UIColor(red: 196/255, green: 196/255, blue: 196/255, alpha: 1)))
+                       .foregroundColor(Color.black)
+                       .cornerRadius(6)
+                       .padding()
+               }
            }.buttonStyle(.bordered)
         }
     }
     
     func selectTime() {
-        print("Time selected")
         let timeInMinutes = startTime.hours*60 + startTime.minutes
         UserDefaults.standard.setTime(value: timeInMinutes)
         UserDefaults.standard.setSetupBool(value: true)
