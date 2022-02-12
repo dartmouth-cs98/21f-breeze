099639307168f6d638d5fa0df4fe6a0daa4f053b
Sabrina Jain <sabrinasjain@gmail.com>
2022-02-12 13:36:55 -0500

    New view for location authorization

---
+++ Breeze/Views/LocationAuthorizationView.swift
@@ -0,0 +1,50 @@
+//
+//  TimeSelectionView.swift
+//  Breeze
+//
+//  Created by Laurel Dernbach on 11/9/21.
+//
+import Foundation
+import SwiftUI
+struct TimeSelectionView: View {
+    
+    @Binding var timeSelectionIsPresenting: Bool
+    @State private var startTime = Time(hours:0, minutes:0, seconds:0)
+    @State private var timeSelected = false;
+
+    var body: some View {
+        ZStack {
+           VStack {
+               Text("One more thing: In order for us to track your screen time, Breeze needs to use location updates to keep running in the background")
+                   .font(.body)
+                   .multilineTextAlignment(.center)
+               // if user has touched the durationPicker twice (work around to apple's bug)
+               if (timeSelected) {
+                   Button("OK", action: authorizeLocationUpdates)
+                       .padding()
+                       .font(.body)
+                       .background(Color.init(UIColor(red: 221/255, green: 247/255, blue: 246/255, alpha: 1)))
+                       .foregroundColor(Color.black)
+                       .cornerRadius(6)
+               }
+               // otherwise, unclickable button
+               else {
+                   Button("Confirm", action: {})
+                       .padding()
+                       .font(.body)
+                       .background(Color.init(UIColor(red: 196/255, green: 196/255, blue: 196/255, alpha: 1)))
+                       .foregroundColor(Color.black)
+                       .cornerRadius(6)
+               }
+           }
+        }
+    }
+    
+    func authorizeLocationUpdates() {
+        let timeInMinutes = startTime.hours*60 + startTime.minutes
+        UserDefaults.standard.setTime(value: timeInMinutes)
+        UserDefaults.standard.setSetupBool(value: true)
+        timeSelectionIsPresenting.toggle()
+    }
+    
+ }
